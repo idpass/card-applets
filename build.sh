2237bdfb08852a76508fd46406b6452c9282c5b5
dexter aparicio <dexter@newlogic.io>
2019-11-21 12:13:02 +0000

    Uncommentingh back the temporary comment

--- build.sh
+++ build.sh
@@ -12,13 +12,13 @@ if [ ! -d libs-sdks ];then
     git clone https://github.com/martinpaljak/oracle_javacard_sdks.git libs-sdks
 fi
 
-#mkdir -p src/main/java/org/idpass
-#
-#t=tmp$$
-#mkdir -p $t
-#git clone git@github.com:idpass/card-tools-applet.git $t/tools
-#git clone git@github.com:idpass/card-auth-applet.git $t/auth
-#git clone git@github.com:idpass/card-sam-applet.git $t/sam
+mkdir -p src/main/java/org/idpass
+
+t=tmp$$
+mkdir -p $t
+git clone git@github.com:idpass/card-tools-applet.git $t/tools
+git clone git@github.com:idpass/card-auth-applet.git $t/auth
+git clone git@github.com:idpass/card-sam-applet.git $t/sam
 
 ln -sf `pwd`/$t/tools/src/org/idpass/tools `pwd`/src/main/java/org/idpass/
 ln -sf `pwd`/$t/auth/src/org/idpass/auth `pwd`/src/main/java/org/idpass/
