67482f85230abc7f4c8a7db3557eeb2e2dcb0149
dexter <dexter@newlogic.io>
2019-11-29 16:16:38 +0800

    Organize gradle build configuration

--- build.sh
+++ build.sh
@@ -1,21 +1,11 @@
 #!/bin/sh
 #
 
-echo "*** environment check ***"
-git branch
-echo */
-ls
-#find . -type f -name '*.java'
-echo "----------oOo---------"
-
 export JC_HOME=$(pwd)/libs-sdks/jc304_kit/
 export _JAVA_OPTIONS=-Djc.home=$JC_HOME
 
-rm -rf build/
-
-./gradlew -b build.gradle.tools build
-./gradlew -b build.gradle.auth convertJavacard
-./gradlew -b build.gradle.sam convertJavacard
+./gradlew clean
+./gradlew tools:build auth:convertJavacard sam:convertJavacard
 
 echo
 echo "*** Generated *.cap files ***"
