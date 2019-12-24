b6b91875f9a6691b95f728ce279352d8ed575080
dx <typelogic@pm.me>
2019-12-24 14:21:20 +0800

    Add check for build outputs

--- build.sh
+++ build.sh
@@ -4,9 +4,23 @@
 export JC_HOME=$(pwd)/libs-sdks/jc304_kit/
 export _JAVA_OPTIONS=-Djc.home=$JC_HOME
 
+buildoutputs='
+build/javacard/org/idpass/auth/javacard/auth.cap
+build/javacard/org/idpass/tools/javacard/tools.cap
+build/javacard/org/idpass/sam/javacard/sam.cap
+build/javacard/org/idpass/datastorage/javacard/datastorage.cap'
+
+buildoutputscount=$(echo $buildoutputs | tr ' ' '\n' | wc -l)
 ./gradlew build
 
-echo
-echo "*** Generated *.cap files ***"
-find build -type f -name '*.cap' -exec md5sum {} \;
-echo
+count=0
+for x in $buildoutputs;do
+    if [ ! -f $x ];then
+        echo "Failed to build $x"
+        continue
+    fi
+    count=$((count+1))
+done
+
+[ "$count" -eq "$buildoutputscount" ] && return 0 || return 1
+
